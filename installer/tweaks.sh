0f0cdbee487e4b13816123b6d6ca41d4e692eaaa
khantaena <khantaena@gmail.com>
2017-02-11 19:25:49 +0700

    installer and uninstaller in same file
    installer and uninstaller in same file

--- installer/tweaks.sh
+++ installer/tweaks.sh
@@ -5,16 +5,55 @@ echo 1 > /sys/class/gpio/Watchdog\ Disable/value
 mount -o rw,remount /
 
 MYDIR=$(dirname "$(readlink -f "$0")")
+CONF_FILE="/data_persist/dev/bin/autorun"
+NAME="autorun"
 
-if [ -e /data_persist/dev/bin/autorun ]; then
- echo "
-  /jci/tools/jci-dialog --title="Ready" --text="AUTORUN exist..Abort" --ok-label='OK' --no-cancel
+. ${MYDIR}/utils.sh
+
+CMU_SW_VER=$(get_cmu_sw_version)
+rm -f "${MYDIR}/AIO_log.txt"
+
+/jci/tools/jci-dialog --3-button-dialog --title="Tweaks Selection for AUTORUN" --text="Choosing Method" --ok-label="Install" --cancel-label="Uninstall" --button3-label="Skip"
+choice=$?
+
+if [ "$choice" = '0' ]; then
+ msg="Install.."
+
+ if [ -e ${CONF_FILE} ]; then
+  # delete empty lines
+  sed -i '/^$/ d' ${CONF_FILE}
+  sed -i '/#!/ a' ${CONF_FILE}
+  if grep -Fq "### ${NAME}" ${CONF_FILE}
+   then
+    echo "exist"
+   else
+    cat ${NAME}.temp >> ${CONF_FILE}
+  fi
+ else
+  echo "#!/bin/sh" > ${CONF_FILE}
+  cat ${NAME}.temp >> ${CONF_FILE}
+ fi
+elif [ "$choice" = '1' ]; then
+ msg="Uninstall.."
+ if [ -e ${CONF_FILE} ]; then
+ # delete empty lines
+ sed -i '/^$/ d' ${CONF_FILE}
+ sed -i '/#!/ a' ${CONF_FILE}
+ if grep -Fq "### ${NAME}" ${CONF_FILE}
+  then
+   t=`cat ${CONF_FILE}.temp`
+   sed -i "/### ${NAME}/,/fi/d" ${CONF_FILE}
+  else
+   echo "Clean"
+ fi
+ else
+  echo "No autorun"
+ fi
+
+ killall jci-dialog
+ reboot
 else
- cp -a ${MYDIR}/autorun /data_persist/dev/bin/autorun
- chmod +x /data_persist/dev/bin/autorun
- chown user:user /data_persist/dev/bin/autorun
- /jci/tools/jci-dialog --title="Ready" --text="Activate autorun completed" --ok-label='OK' --no-cancel
+ msg="Abort.."
+ show_message "INSTALLATION ABORTED PLEASE UNPLUG USB DRIVE"
 fi
-
-mount -o ro,remount /
 
