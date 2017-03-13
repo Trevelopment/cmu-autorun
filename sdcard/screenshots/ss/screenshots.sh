82f242a20c8a8c6ccb07acee2f64d020657e762f
Trevor Martin <Trez@mazdatweaks.com>
2017-03-12 20:59:09 -0700

    Screen 💉

--- sdcard/screenshots/ss/screenshots.sh
+++ sdcard/screenshots/ss/screenshots.sh
@@ -1,29 +1,43 @@
 #!/bin/sh
+# Screneenshots. By: Trezdog44
+# Takes X screenshots over Y Seconds (set in screenshots.conf)
+show_message()
+{
+	sleep 5
+	killall jci-dialog
+	/jci/tools/jci-dialog --info --title="SCREENSHOTS BRO" --text="$*" --no-cancel &
+}
 MYDIR=$(dirname "$(readlink -f "$0")")
 CONFIGFILE="${MYDIR}/screenshots.conf"
-SDDIR="/tmp/mnt/sd_nav/"
 SSGAP=30
 NUMSCRNSHOTS=10
-DONE_MESSAGE=0
+DONE_MESSAGE=1
+MAX=0
 . ${CONFIGFILE}
 # echo "=${NUMSCRNSHOTS}= =${SSGAP}= =${DONE_MESSAGE}=" >> ${MYDIR}/sstest.log
 sleep 60
-if [ ! -e ${SDDIR}/screenshots ]
+if [ "${DONE_MESSAGE}" = "1" ]
 then
-	mkdir -p ${SDDIR}/screenshots
+	show_message "${NUMSCRNSHOTS} SCREENSHOTS"
+	sleep 5
+	killall jci-dialog
+fi
+if [ ! -e ${MYDIR}/screenshots ]
+then
+	mkdir -p ${MYDIR}/screenshots
 fi
 for i in $(seq 1 $NUMSCRNSHOTS)
 do
 	/usr/bin/screenshot
 	sleep ${SSGAP}
-	cp /wayland-screenshot.png ${SDDIR}/screenshots/screenshot${i}.png
+	cp /wayland-screenshot.png ${MYDIR}/screenshots/screenshot${i}.png
+	#echo "${MYDIR}/screenshots/screenshot$((i+MAX)).png" >> ${MYDIR}/ss.log
 	sleep ${SSGAP}
 done
-if [ ${DONE_MESSAGE} = 1 ]
+if [ "${DONE_MESSAGE}" = "1" ]
 then
-	killall jci-dialog
-	/jci/tools/jci-dialog --info --title="AIO-SCREENSHOTS" --text="${NUMSCRNSHOTS} SCREENSHOTS TAKEN" --no-cancel &
-	sleep 3
+	show_message "${NUMSCRNSHOTS} SCREENSHOTS TAKEN"
+	sleep 15
 	killall jci-dialog
 fi
-exit
+
